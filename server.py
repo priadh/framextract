a7f33b56ed295f7560cda5bdf34b4d43d9038c61
priadh <135139175+priadh@users.noreply.github.com>
2025-11-20 16:14:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -162,5 +162,5 @@ async def extract_upload(
 # ===============================
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 7860))
     uvicorn.run(app, host="0.0.0.0", port=port)
